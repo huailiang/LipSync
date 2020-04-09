84164970753eccadd8432b23ccce9174013b474c
penghuailiang <penghuailiang@126.com>
2020-04-09 18:32:28 +0800

    lip sync editor

--- Assets/LipSyncLite/Editor/LipSyncEditor.cs
+++ Assets/LipSyncLite/Editor/LipSyncEditor.cs
@@ -3,11 +3,68 @@ using UnityEngine;
 
 namespace LipSync
 {
-    [CustomEditor(typeof(AudioLipSync))]
-    [CanEditMultipleObjects]
     public class LipSyncEditor : Editor
     {
-        private bool isAdvancedOptionsFoldOut;
+
+        protected bool isAdvancedOptionsFoldOut;
+
+        protected void GUIVowel(LipSync sync)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("recognizerLanguage"));
+            string[] selectedVowels = null;
+            switch (sync.recognizerLanguage)
+            {
+                case ERecognizerLanguage.Japanese:
+                    selectedVowels = LipSync.vowelsJP;
+                    break;
+                case ERecognizerLanguage.Chinese:
+                    selectedVowels = LipSync.vowelsCN;
+                    break;
+            }
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("targetBlendShapeObject"));
+            EditorGUILayout.LabelField("Vowel Property Names");
+            EditorGUILayout.BeginVertical(EditorStyles.textField);
+            {
+                SerializedProperty propertyNames = serializedObject.FindProperty("propertyNames");
+                for (int i = 0; i < selectedVowels.Length; ++i)
+                {
+                    EditorGUILayout.PropertyField(propertyNames.GetArrayElementAtIndex(i), new GUIContent(selectedVowels[i]));
+                }
+            }
+            EditorGUILayout.EndVertical();
+        }
+
+        protected void GUIAdvanceOptions(LipSync sync)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("propertyMinValue"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("propertyMaxValue"));
+            EditorGUILayout.Space();
+            isAdvancedOptionsFoldOut = EditorGUILayout.Foldout(isAdvancedOptionsFoldOut, "Advanced Options");
+            if (isAdvancedOptionsFoldOut)
+            {
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("windowSize"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("amplitudeThreshold"));
+                EditorGUILayout.PropertyField(serializedObject.FindProperty("moveTowardsSpeed"));
+            }
+            EditorGUILayout.Space();
+
+            if (Application.isPlaying)
+            {
+                EditorGUILayout.HelpBox("Changes of settings at runtime must be applied manually using the button below.", MessageType.Warning);
+                if (GUILayout.Button("Apply runtime changes"))
+                {
+                    sync.InitializeRecognizer();
+                }
+            }
+        }
+    }
+
+
+    [CustomEditor(typeof(AudioLipSync))]
+    [CanEditMultipleObjects]
+    public class AudioLipSyncEditor : LipSyncEditor
+    {
 
         public override void OnInspectorGUI()
         {
@@ -19,52 +76,8 @@ namespace LipSync
             EditorGUILayout.Space();
             if (targetLipSync.lipSyncMethod == ELipSyncMethod.Runtime)
             {
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("recognizerLanguage"));
-                string[] selectedVowels = null;
-                switch (targetLipSync.recognizerLanguage)
-                {
-                    case ERecognizerLanguage.Japanese:
-                        selectedVowels = AudioLipSync.vowelsJP;
-                        break;
-                    case ERecognizerLanguage.Chinese:
-                        selectedVowels = AudioLipSync.vowelsCN;
-                        break;
-                }
-
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("targetBlendShapeObject"));
-                EditorGUILayout.LabelField("Vowel Property Names");
-                EditorGUILayout.BeginVertical(EditorStyles.textField);
-                {
-                    SerializedProperty propertyNames = serializedObject.FindProperty("propertyNames");
-                    for (int i = 0; i < selectedVowels.Length; ++i)
-                    {
-                        EditorGUILayout.PropertyField(propertyNames.GetArrayElementAtIndex(i), new GUIContent(selectedVowels[i]));
-                    }
-                }
-                EditorGUILayout.EndVertical();
-
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("propertyMinValue"));
-                EditorGUILayout.PropertyField(serializedObject.FindProperty("propertyMaxValue"));
-
-                EditorGUILayout.Space();
-
-                isAdvancedOptionsFoldOut = EditorGUILayout.Foldout(isAdvancedOptionsFoldOut, "Advanced Options");
-                if (isAdvancedOptionsFoldOut)
-                {
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("windowSize"));
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("amplitudeThreshold"));
-                    EditorGUILayout.PropertyField(serializedObject.FindProperty("moveTowardsSpeed"));
-                }
-                EditorGUILayout.Space();
-
-                if (Application.isPlaying)
-                {
-                    EditorGUILayout.HelpBox("Changes of settings at runtime must be applied manually using the button below.", MessageType.Warning);
-                    if (GUILayout.Button("Apply runtime changes"))
-                    {
-                        targetLipSync.InitializeRecognizer();
-                    }
-                }
+                GUIVowel(targetLipSync);
+                GUIAdvanceOptions(targetLipSync);
             }
             else if (targetLipSync.lipSyncMethod == ELipSyncMethod.Baked)
             {
@@ -76,6 +89,27 @@ namespace LipSync
                     window.Show();
                 }
             }
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+
+    [CustomEditor(typeof(FmodLipSync))]
+    [CanEditMultipleObjects]
+    public class FmodLipSyncEditor : LipSyncEditor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            FmodLipSync targetLipSync = (FmodLipSync)target;
+            serializedObject.UpdateIfRequiredOrScript();
+
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("emiter"));
+            EditorGUILayout.Space();
+            
+            GUIVowel(targetLipSync);
+            GUIAdvanceOptions(targetLipSync);
 
             serializedObject.ApplyModifiedProperties();
         }
